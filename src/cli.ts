38438b397c9e4bb9e62bada080ca6e4cb846908a
kiliman <kiliman@gmail.com>
2022-05-04 09:54:54 -0400

    🐛 Fix commandPath for Windows
    Fixes #1

--- src/cli.ts
+++ src/cli.ts
@@ -12,9 +12,10 @@ async function main() {
   }
   const cliPath = path.dirname(fs.realpathSync(process.argv[1]))
   const commandName = process.argv[2]
-  const command = (
-    await import(path.resolve(cliPath, `./commands/${commandName}.js`))
-  ).default
+  // add file:// prefix for windows imports
+  const commandPath =
+    'file://' + path.join(cliPath, 'commands', `${commandName}.js`)
+  const command = (await import(commandPath)).default
   const args = process.argv.slice(3)
   await command.default(args)
 }
