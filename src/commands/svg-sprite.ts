41b776de645f923ee70bd84d970b34fac50dec1a
kiliman <kiliman@gmail.com>
2023-07-21 17:09:57 -0400

    🔨 Update CLI to specify sprite and types filenames

--- src/commands/svg-sprite.ts
+++ src/commands/svg-sprite.ts
@@ -5,9 +5,11 @@ import svgParser from '../libs/svg-parser'
 
 let rootFolder = ''
 let outputFolder = ''
-let outputFilename = 'index.tsx'
 let template: string | null = null
 let namedComponents = false
+let component = 'icon.tsx'
+let sprite = 'icon.svg'
+let types = ''
 
 export default function (args: string[]) {
   // verify arguments
@@ -17,7 +19,7 @@ export default function (args: string[]) {
     // if output folder has extension, then treat this as the base name
     if (outputPath.endsWith('.tsx')) {
       outputFolder = path.dirname(outputPath)
-      outputFilename = path.basename(outputPath)
+      component = path.basename(outputPath)
     } else {
       outputFolder = normalizeFolder(outputPath)
     }
@@ -25,21 +27,36 @@ export default function (args: string[]) {
       if (args[i].startsWith('--template=')) {
         const templateFilename = args[i].substring('--template='.length)
         template = fs.readFileSync(templateFilename, 'utf8')
+      } else if (args[i].startsWith('--component=')) {
+        sprite = args[i].substring('--component='.length)
+      } else if (args[i].startsWith('--sprite=')) {
+        sprite = args[i].substring('--sprite='.length)
+      } else if (args[i].startsWith('--types=')) {
+        types = args[i].substring('--types='.length)
       } else if (args[i] === '--components') {
         namedComponents = true
       }
     }
   } else {
     console.log(
-      'Usage: npx rmx-cli svg-sprite SOURCE_FOLDER OUTPUT_PATH [--components] [--template=TEMPLATE_FILE]',
+      `
+Usage: npx rmx-cli svg-sprite SOURCE_FOLDER OUTPUT_PATH
+               [--sprite=FILENAME] [--types=FILENAME]
+               [--components] [--template=TEMPLATE_FILE]
+
+SOURCE_FOLDER: folder containing .svg files
+OUTPUT_PATH: output path for sprite file and components
+
+* If OUTPUT_PATH ends with .tsx, then use this as the base filename
+  (default: icon.tsx)
+
+--sprite=FILENAME: base filename of sprite file (default: icon.svg)
+--types=FILENAME : base filename of IconType export file
+                   if present, will not generate component file
+--components     : generate named components for each icon
+--template=TEMPLATE_FILE: use custom template file
+`.trimStart(),
     )
-    console.log('  SOURCE_FOLDER: folder containing .svg files')
-    console.log('  OUTPUT_PATH: output path for sprite file and components')
-    console.log(
-      '    if OUTPUT_PATH ends with .tsx, then use this as the base filename',
-    )
-    console.log('   --components: generate named components for each icon')
-    console.log('  --template=TEMPLATE_FILE: use custom template file')
     process.exit(1)
   }
 
@@ -53,6 +70,7 @@ function log(...args: any[]) {
   logs.push(args)
 }
 function flushLogs() {
+  if (logs.length === 0) return
   logs.forEach(args => console.log(...args))
   logs.length = 0
 }
@@ -90,9 +108,10 @@ function generateSprite(folder: string, files: string[]) {
   // spriteOutputFolder: components/svg/heroicons/20/solid
 
   const spriteOutputFolder = folder.replace(rootFolder, outputFolder)
+
   const spriteOutput = path.join(
     spriteOutputFolder,
-    path.basename(outputFilename, '.tsx') + '.svg',
+    path.basename(component, '.tsx') + '.svg',
   )
 
   log(`📝 Generating sprite for ${folder}`)
@@ -123,13 +142,43 @@ function generateSprite(folder: string, files: string[]) {
     flushLogs()
   }
 
+  if (types) {
+    generateTypesFile(spriteOutputFolder, files)
+    return
+  }
+
   generateReactComponent(spriteOutputFolder, files)
 }
 
+function generateTypesFile(spriteOutputFolder: string, files: string[]) {
+  let icons = files.map(file => path.basename(file, '.svg'))
+  let output = `export const iconNames = [
+${icons.map(icon => `  "${icon}",`).join('\n')}
+] as const;
+
+export type IconName = typeof iconNames[number];
+`
+  icons.forEach(icon => log(`✅ ${icon}`))
+
+  let typesPath = path.join(spriteOutputFolder, types)
+  const exists = fs.existsSync(typesPath)
+  let hasChanges = !exists // if types doesn't exist, then we have changes
+  if (exists) {
+    // read existing types file
+    const existing = fs.readFileSync(typesPath, 'utf8')
+    if (existing !== output) {
+      hasChanges = true
+    }
+  }
+  if (hasChanges) {
+    fs.writeFileSync(typesPath, output, 'utf8')
+    flushLogs()
+  }
+}
 function generateReactComponent(spriteOutputFolder: string, files: string[]) {
   let icons = files.map(file => path.basename(file, '.svg'))
-  const spritePath = path.basename(outputFilename, '.tsx') + '.svg'
-  let component =
+  const spritePath = path.basename(component, '.tsx') + '.svg'
+  let output =
     template ??
     `
 import { type SVGProps } from "react";
@@ -144,13 +193,15 @@ export default function Icon({ icon, ...props}: SVGProps<SVGSVGElement> & { icon
   );
 }
 `
-  // add type IconName for each icon file
-  component += `
+  // if user didn't specify types file then inline them
+  if (!types) {
+    // add type IconName for each icon file
+    output += `
 export const iconNames = [
 ${icons.map(icon => `  "${icon}",`).join('\n')}
 ] as const;
 export type IconName = typeof iconNames[number];`
-
+  }
   icons.forEach(icon => log(`✅ ${icon}`))
 
   // if user wants named components, generate them
@@ -164,21 +215,21 @@ export type IconName = typeof iconNames[number];`
 export const ${componentName}Icon = (props: SVGProps<SVGSVGElement>) => <Icon icon="${icon}" {...props} />;`
     })
   }
-  const componentOutput = path.join(spriteOutputFolder, outputFilename)
-  component = component.trim()
+  const componentPath = path.join(spriteOutputFolder, component)
+  output = output.trimStart()
 
-  const exists = fs.existsSync(componentOutput)
+  const exists = fs.existsSync(componentPath)
   let hasChanges = !exists // if sprite doesn't exist, then we have changes
   if (exists) {
     // read existing sprite file
-    const existingComponent = fs.readFileSync(componentOutput, 'utf8')
-    if (existingComponent !== component) {
+    const existing = fs.readFileSync(componentPath, 'utf8')
+    if (existing !== output) {
       hasChanges = true
     }
   }
   if (hasChanges) {
     // write sprite file
-    fs.writeFileSync(componentOutput, component, 'utf8')
+    fs.writeFileSync(componentPath, output, 'utf8')
     flushLogs()
   }
 }
